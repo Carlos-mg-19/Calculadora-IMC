16760cfd85d5a8937ffb0c684b0cd6c731485cb8
Carlos-mg-19 <115847662+Carlos-mg-19@users.noreply.github.com>
2023-07-14 18:28:55 -0300

    commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let altura = document.getElementById('altura')
+let peso = document.getElementById('kilos')
+let resultado = document.getElementById('res')
+
+
+
+function calcular(){
+   if (altura.value.length == 0 || peso.value.length == 0 ){
+    window.alert('Preencha todos os dados')
+   }else{
+    let A = Number(altura.value)
+    let P = Number(peso.value)
+    let imc = (P / (A * A))
+    
+    resultado.innerHTML= (` Resultado:${imc.toFixed(1)}`)
+   }
+}
+
+
+
